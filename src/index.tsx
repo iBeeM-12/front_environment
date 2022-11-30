4137caeafd5f1179a7112977b2ef2c86afb2751c
ren <rei155979@gmail.com>
2022-11-30 20:44:44 +0900

    initialcommit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
