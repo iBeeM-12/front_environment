fcee5ba201541b317673d5d493de5990a2b90e62
zo-ma <zero.one.mean.ability@outlook.jp>
2022-12-09 00:22:43 +0900

    ダミーデータにグループ画像登録しておきました

--- src/components/GroupTop.tsx
+++ src/components/GroupTop.tsx
@@ -1,14 +1,23 @@
-import { Box } from "@chakra-ui/react";
-import { GroupList } from "../data/dummyData";
+import { Box, Center } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
+import { GroupInfo0 } from "../data/dummyData";
 import { HStack, Text } from "@chakra-ui/react";
 
 export const GroupTop = () => {
   return (
     <>
       {/* FIXME: スタイルをピクセルで指定しているためもう少しいい感じにする */}
-      <HStack bg="#F6C745" h={"150px"} spacing={10}>
+      <HStack bg="#F6C745" h={"150px"} spacing={10} key={GroupInfo0[1]}>
+        <Box w={"150px"} h={"150px"}>
+          <Image src={GroupInfo0[0]} alt="name1" />
+        </Box>
+
         <Box>
-          <Text fontSize="3xl">{GroupList[0]}</Text>
+          <Text fontSize="3xl">{GroupInfo0[1]}</Text>
+          <Center>
+            <Avatar name="temp1" src={GroupInfo0[2]} />
+          </Center>
         </Box>
       </HStack>
     </>
