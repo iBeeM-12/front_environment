3062dbde6e9b85e98d45ed86a5c4945144c1724b
ren <rei155979@gmail.com>
2022-12-09 21:32:46 +0900

    Tab以外の組み上げ

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { MemberView } from "./MemberView";
+import { GroupTop } from "./GroupTop";
+import { VStack } from "@chakra-ui/react";
 
 export const Group = () => {
   return (
     <>
+      <GroupTop />
       <MemberView />
     </>
   );
--- src/components/MemberView.tsx
+++ src/components/MemberView.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Stack, VStack } from "@chakra-ui/react";
+import { Box, HStack, Image, Stack, VStack } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 
@@ -37,16 +37,18 @@ const MemberList = [
 
 export const MemberView = () => {
   return (
-    <VStack marginRight={100}>
-      {MemberList.map((member) => {
-        return (
-          <HStack key={member.name} spacing={5}>
-            <Image w="160px" h="90px" src={member.smn} alt="samune" />
-            <Avatar src={member.stat} />
-            <Text>{member.name}</Text>
-          </HStack>
-        );
-      })}
-    </VStack>
+    <Box bg={"#E5D1C8"}>
+      <VStack marginRight={100}>
+        {MemberList.map((member) => {
+          return (
+            <HStack key={member.name} spacing={5}>
+              <Image w="160px" h="90px" src={member.smn} alt="samune" />
+              <Avatar src={member.stat} />
+              <Text>{member.name}</Text>
+            </HStack>
+          );
+        })}
+      </VStack>
+    </Box>
   );
 };
