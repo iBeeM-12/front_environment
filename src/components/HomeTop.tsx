cdd6f9e722a6a995b9c06b80074d56de0d4988f5
ren <rei155979@gmail.com>
2022-12-10 18:23:09 +0900

    ページの遷移（仮）

--- src/components/HomeTop.tsx
+++ src/components/HomeTop.tsx
@@ -3,16 +3,25 @@ import { Image } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
 import { MemberInfo0 } from "../data/dummyData";
 import { HStack, Text, Center } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 //ホーム画面上部のコンポーネント
 
 export const HomeTop = () => {
+  const navigate = useNavigate();
   return (
     <>
       {/* FIXME: スタイルをピクセルで指定しているためもう少しいい感じにする */}
       <HStack bg="#F6C745" h={"150px"} spacing={10} key={MemberInfo0[1]}>
         <Box w={"240px"} h={"150px"}>
-          <Image src={MemberInfo0[0]} alt="name1" />
+          <Image
+            src={MemberInfo0[0]}
+            alt="name1"
+            onClick={() => {
+              // console.log("ositeruyo");
+              navigate("/profile");
+            }}
+          />
         </Box>
 
         <Box>
