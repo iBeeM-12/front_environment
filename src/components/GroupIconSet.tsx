364cafaf1e1b39463bc09edce5463abb4acca3c0
zo-ma <zero.one.mean.ability@outlook.jp>
2022-12-10 12:18:04 +0900

    テキストとアイコン

---
+++ src/components/GroupIconSet.tsx
@@ -0,0 +1,18 @@
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { GroupInfo0 } from "../data/dummyData";
+import React from "react";
+
+type Props = {
+  text: string;
+};
+export const GroupIconSet = ({ text }: Props) => {
+  return (
+    <Stack spacing={3}>
+      <Text fontSize="2xl">グループ名：</Text>
+      <Text fontSize="2xl">グループアイコンを設定</Text>
+      <Box w={"240px"} h={"135px"}>
+        <Image src={GroupInfo0[0]} alt="name1" />
+      </Box>
+    </Stack>
+  );
+};
