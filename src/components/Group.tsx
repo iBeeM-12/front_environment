6c69cbbf0dfbee38df3c883d6202f141431bfb9b
kinefuchi <kinesan928@gmail.com>
2022-12-10 16:29:32 +0900

    ちょっときれいにした

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MemberView } from "./MemberView";
 import { GroupTop } from "./GroupTop";
-import { VStack } from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
 import { Tab1 } from "./Tab";
 
 export const Group = () => {
@@ -9,6 +9,7 @@ export const Group = () => {
     <>
       <VStack bg="#F6C745" spacing={1}>
         <GroupTop />
+        <Box h={"10px"}></Box>
         <Tab1 />
       </VStack>
     </>
