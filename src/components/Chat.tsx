d9abe442671198b9188a8b7c4911a43fd42e961d
kinefuchi <kinesan928@gmail.com>
2023-02-21 12:33:46 +0900

    アイコンだけ表示

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,11 +3,13 @@ import { GroupTop } from "./GroupTop";
 import { Box, VStack } from "@chakra-ui/react";
 import { Stampver } from "./StampSelect";
 import { Tab2 } from "./Tab";
+import { ChatLeftIcon } from "./ChatLeftIcon";
 
 export const Chat = () => {
   return (
     <>
       <VStack bg="#F6C745" spacing={1}>
+        <ChatLeftIcon />
         <Tab2 />
       </VStack>
     </>
---
+++ src/components/ChatLeftIcon.tsx
@@ -0,0 +1,19 @@
+import { Image } from "@chakra-ui/react";
+import { MemberInfo0 } from "../data/dummyData";
+
+//欲しいデータ　グループ内の誰が喋ったのか、icon画像のURK(繰り返しは外で実施する方がいい説)
+//渡すデータなし(出力のみ)
+export const ChatLeftIcon = () => {
+  return (
+    <>
+      <Image
+        borderRadius="full"
+        boxSize="50px"
+        //喋った人の画像
+        src={MemberInfo0[0]}
+        //喋った人の名前(これがキーになるイメージ)
+        alt={MemberInfo0[1]}
+      />
+    </>
+  );
+};
