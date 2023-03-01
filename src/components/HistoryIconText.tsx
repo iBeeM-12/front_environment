30bab773a88628431a60bf31c5d478fe3ef659e5
kinefuchi <kinesan928@gmail.com>
2023-03-02 00:19:42 +0900

    初めて最後まで出来た可能性

--- src/components/HistoryIconText.tsx
+++ src/components/HistoryIconText.tsx
@@ -1,13 +1,37 @@
 import { HStack, Text } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
-import { MemberInfo1 } from "../data/dummyData";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 //プロフィール画面の履歴のアイコンとテキスト
 export const HistoryIconText = () => {
+  const [historys, setHistorys] = useState<[string, string]>();
+
+  useEffect(() => {
+    const url_historys =
+      "http://localhost:8000/home/user/get_user_record?user_id=1";
+    axios
+      .get(url_historys)
+      .then((res) => {
+        setHistorys([res.data[0], res.data[1]]);
+      })
+
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
   return (
-    <HStack>
-      <Avatar src={MemberInfo1[2]} />
-      <Text fontSize="xl"> ○○:○○表示を開始しました</Text>
-    </HStack>
+    <>
+      {historys &&
+        historys.map((history) => {
+          return (
+            <HStack>
+              <Avatar key={history[1]} src={history[0]} />
+              <Text fontSize="xl"> {`${history[1]}`}表示を開始しました</Text>
+            </HStack>
+          );
+        })}
+    </>
   );
 };
