87975f3cbe1a2a940c5ee2fab2571fc47b8b8eba
KEiJey <keijey1015@outlook.jp>
2023-03-03 22:25:53 +0900

    refactor: 関数をコンポーネント化

--- src/components/ChatLeftIcon.tsx
+++ src/components/ChatLeftIcon.tsx
@@ -1,5 +1,5 @@
-import { Image } from "@chakra-ui/react";
-import { MemberInfo0 } from "../data/dummyData";
+import { Box, Image } from "@chakra-ui/react";
+import { ChatList, MemberInfo0 } from "../data/dummyData";
 
 //欲しいデータ　グループ内の誰が喋ったのか、icon画像のURK(繰り返しは外で実施する方がいい説)
 //渡すデータなし(出力のみ)
