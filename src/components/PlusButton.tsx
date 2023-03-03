7266aff541c772e76ce5862b68d9893bfdea3485
kinefuchi <kinesan928@gmail.com>
2023-03-03 21:28:00 +0900

    Co-authored-by: Toshiki Sugimizu <ltoppyl@users.noreply.github.com>

--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -24,16 +24,21 @@ export const PlusButtonGroup = ({ text }: Props) => {
   );
 };
 
-export const PlusButtonMember = ({ text }: Props) => {
+type propsMember = {
+  text: string;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export const PlusButtonMember = ({ text, setState }: propsMember) => {
   const navigate = useNavigate();
   return (
     <HStack>
       <Icon
         as={PlusSquareIcon}
         boxSize={9}
-        //onClick={() => {
-        //navigate("/group/create");
-        //}}
+        onClick={
+          () => setState(true)
+          // もし、trueだったら、Tabの<p></p>が描画が変わる
+        }
       />
       <Text fontSize="xl">{text}</Text>
     </HStack>
